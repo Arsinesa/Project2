509aea8f4c274bd8ed21ef61ac2652ee07ab76f8
Arsinesa <arsines@internet.ru>
2023-11-29 23:15:26 +0300

    Initial commit

--- src/NetworkScanner.java
+++ src/NetworkScanner.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class NetworkScanner {
-}
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NetworkScanner {
+
+    public static List <String> NetworkScanner(){
+        List <String> activeIPs= new ArrayList<String>();
+        try {
+            String localHostAddress = InetAddress.getLocalHost().getHostAddress();
+            String[] LocalHostAddressParts = localHostAddress.split("//.");
+            for (int i = 1; i<255; i++){
+                String ipAddress = LocalHostAddressParts[0]+"." +
+                        ""+LocalHostAddressParts[1]+"."+LocalHostAddressParts[2]+"."+i;
+                        if (InetAddress.getByName(ipAddress).isReachable(1000)){
+                            activeIPs.add(ipAddress);
+
+                        }
+            }
+
+        } catch (UnknownHostException e){
+            System.out.println("Error: "+ e.getMessage());}
+        catch (Exception e) {
+            System.out.println("Error:"+ e.getMessage());}
+        return activeIPs;
+        }
+    }
+
